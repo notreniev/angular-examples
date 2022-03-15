f84b2fc16b04bf2a1dd875b5ce1daf091f3da131
notreniev <everton.canez@gmail.com>
2022-03-14 21:02:23 -0300

    wip: added eslint rules

--- src/app/components/counter-component/counter-component.component.ts
+++ src/app/components/counter-component/counter-component.component.ts
@@ -1,14 +1,17 @@
-import { ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit } from '@angular/core';
-import { Observable } from 'rxjs';
+import {
+  ChangeDetectionStrategy, ChangeDetectorRef, Component, Input, OnInit,
+} from "@angular/core";
+import { Observable } from "rxjs";
 
 @Component({
-  selector: 'app-counter',
-  templateUrl: './counter-component.component.html',
-  styleUrls: ['./counter-component.component.css'],
-  changeDetection: ChangeDetectionStrategy.OnPush
+  selector: "app-counter",
+  templateUrl: "./counter-component.component.html",
+  styleUrls: ["./counter-component.component.css"],
+  changeDetection: ChangeDetectionStrategy.OnPush,
 })
 export class CounterComponent implements OnInit {
   @Input() data$: Observable<any>;
+
   data;
 
   constructor(private changeDetection: ChangeDetectorRef) { }
@@ -16,8 +19,7 @@ export class CounterComponent implements OnInit {
   ngOnInit(): void {
     this.data$.subscribe((value) => {
       this.data = value;
-      this.changeDetection.markForCheck()
-    })
+      this.changeDetection.markForCheck();
+    });
   }
-
 }
